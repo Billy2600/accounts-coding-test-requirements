61154eca598c47e2ffa4a9800789aceec2808876
Billy <billymcfly@gmail.com>
2020-02-14 12:52:25 -0700

    Add angular project, and account view component

---
+++ accounts-coding-test-requirements/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AccountViewComponent } from './account-view/account-view.component'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'accounts-coding-test-requirements';
+}
